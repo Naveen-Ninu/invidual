e18dcae19dd75f1c56fdbbaf1bc07e440139ad56
Naveen-Ninu <naveenninu888@gmail.com>
2023-01-31 16:32:28 +0530

    add-section

---
+++ assets/js/index.js
@@ -0,0 +1,27 @@
+$(".autoplay").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  autoplay: true,
+  arrows: false,
+  autoplaySpeed: 2500,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 3,
+      },
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        arrows: false,
+        centerMode: true,
+        centerPadding: "40px",
+        slidesToShow: 1,
+      },
+    },
+  ],
+});
---
+++ assets/js/preloder.js
@@ -0,0 +1,28 @@
+//----------- PRELOADER-----------------
+$("body").addClass("overflow-y-hidden");
+$("body").addClass("preloader-class");
+setTimeout(function () {
+  $("#preloader").fadeToggle();
+  $("body").removeClass("overflow-y-hidden");
+  $("body").removeClass("preloader-class");
+}, 3000);
+
+// ===============BACK-TO-TOP=================
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      $("#scroll").fadeIn();
+    } else {
+      $("#scroll").fadeOut();
+    }
+  });
+  $("#scroll").click(function () {
+    $("html, body").animate(
+      {
+        scrollTop: 0,
+      },
+      300
+    );
+    return false;
+  });
+});
