7cab1a33bc34d6ae2e6ced49a9edbf750e6cd71e
Naveen-Ninu <naveenninu888@gmail.com>
2023-02-01 12:45:25 +0530

    all-pages-spacings slider settings

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,27 +1,52 @@
 $(".autoplay").slick({
-  slidesToShow: 1,
-  slidesToScroll: 1,
   autoplay: true,
-  arrows: false,
+  centerMode: true,
+  centerPadding: "110px",
   autoplaySpeed: 2500,
+  dots: false,
+  infinite: true,
+  arrows: false,
+  speed: 1000,
+  slidesToShow: 1.7,
   responsive: [
     {
-      breakpoint: 768,
+      breakpoint: 1200,
       settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: "40px",
-        slidesToShow: 3,
+        slidesToShow: 1,
+        centerPadding: "140px",
       },
     },
     {
-      breakpoint: 480,
+      breakpoint: 576,
       settings: {
-        arrows: false,
-        centerMode: true,
-        centerPadding: "40px",
+        centerPadding: "10px",
         slidesToShow: 1,
       },
     },
   ],
+});
+
+$(".vertical_slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 3,
+  centerMode: true,
+  centerPadding: "120px",
+  autoplay: true,
+  arrows: false,
+  autoplaySpeed: 2500,
+  vertical: true,
+  verticalswaping: true,
+  asNavFor: ".vertical_image_slider",
+});
+$(".vertical_image_slider").slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: "25px",
+  autoplay: true,
+  arrows: false,
+  fade: true,
+  asNavFor: ".vertical_slider",
+  autoplaySpeed: 2500,
+  focusOnSelect: true,
 });
--- assets/js/preloder.js
+++ assets/js/preloder.js
@@ -1,11 +1,11 @@
 //----------- PRELOADER-----------------
-$("body").addClass("overflow-y-hidden");
-$("body").addClass("preloader-class");
-setTimeout(function () {
-  $("#preloader").fadeToggle();
-  $("body").removeClass("overflow-y-hidden");
-  $("body").removeClass("preloader-class");
-}, 3000);
+// $("body").addClass("overflow-y-hidden");
+// $("body").addClass("preloader-class");
+// setTimeout(function () {
+//   $("#preloader").fadeToggle();
+//   $("body").removeClass("overflow-y-hidden");
+//   $("body").removeClass("preloader-class");
+// }, 3000);
 
 // ===============BACK-TO-TOP=================
 $(document).ready(function () {
